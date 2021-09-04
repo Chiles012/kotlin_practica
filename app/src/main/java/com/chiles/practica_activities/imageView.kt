a035b883adc04aca31b9bad2048894c833a45a5f
Chiles012 <sersan.christian@gmail.com>
2021-09-03 21:40:32 -0500

    image layout

---
+++ app/src/main/java/com/chiles/practica_activities/imageView.kt
@@ -0,0 +1,11 @@
+package com.chiles.practica_activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class imageView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_view)
+    }
+}
