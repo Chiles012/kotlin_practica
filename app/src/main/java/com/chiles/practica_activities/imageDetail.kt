f3ed5e776790dae0b1c553bad94f230f3eaa53e7
Chiles012 <sersan.christian@gmail.com>
2021-09-03 21:32:05 -0500

    imageDetail layout

---
+++ app/src/main/java/com/chiles/practica_activities/imageDetail.kt
@@ -0,0 +1,11 @@
+package com.chiles.practica_activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class imageDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_detail)
+    }
+}
