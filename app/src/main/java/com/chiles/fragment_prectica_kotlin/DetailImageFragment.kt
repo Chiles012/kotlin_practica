0de4b6416078e85979c3d5ac2ae697152879a8f8
Chiles012 <sersan.christian@gmail.com>
2021-09-11 14:31:54 -0500

    open details and get picture from carousel

--- app/src/main/java/com/chiles/fragment_prectica_kotlin/DetailImageFragment.kt
+++ app/src/main/java/com/chiles/fragment_prectica_kotlin/DetailImageFragment.kt
@@ -5,7 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 class DetailImageFragment : Fragment(R.layout.fragment_detail_image) {
+
+    lateinit var picture: Picture
+    lateinit var imagen: ImageView
+    lateinit var txtInfo: TextView
+    lateinit var favourite: ImageView
+    lateinit var sound: ImageView
+
+    override fun onResume() {
+        super.onResume()
+        picture = requireArguments().getParcelable<Picture>("picture")!!
+
+        initViews()
+        events()
+    }
+
+    private fun events() {
+        imagen.setOnClickListener {
+            val act = activity
+            if ( act is MainActivity )
+                act.changeFragment(ImageViewFragment(), picture)
+            else
+                Toast.makeText(context, "Error", Toast.LENGTH_LONG)
+        }
+    }
+
+    private fun initViews() {
+        imagen = requireView().findViewById(R.id.imgViewImage)
+        txtInfo = requireView().findViewById(R.id.txtDescription)
+        favourite = requireView().findViewById(R.id.imgLike)
+        sound = requireView().findViewById(R.id.imgMusicLike)
+
+        imagen.setImageResource(picture.sourceImage)
+    }
 
 }
